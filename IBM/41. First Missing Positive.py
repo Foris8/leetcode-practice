65cb4ee9c7702b930abb493ea7bed514ee2378b2
Foris8 <foristang@gmail.com>
2024-02-12 16:22:32 -0600

    41

---
+++ IBM/41. First Missing Positive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        for i in range(len(nums)):
+            while nums[i] < len(nums) and nums[i] > 0 and nums[i] != nums[nums[i] - 1]:
+
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+
+        return nums[-1] + 1
